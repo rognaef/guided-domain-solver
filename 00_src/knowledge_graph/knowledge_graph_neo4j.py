be3099934745853b05239937d286c05f13f99da7
RogNaef <RogNaef@gmail.com>
2025-04-24 18:33:14 +0200

    Added read/write for Neo4J client

--- 00_src/knowledge_graph/knowledge_graph_neo4j.py
+++ 00_src/knowledge_graph/knowledge_graph_neo4j.py
@@ -6,15 +6,49 @@ from neo4j import GraphDatabase
 class KnowledgeGraphNeo4J():
     uri : str
     auth : tuple[str, str]
+    database : str
 
     def __init__(self) -> None:
         load_dotenv(dotenv_path=Path(Path(os.path.dirname(__file__)).parent, ".env"))
         # URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
         self.uri = os.getenv("NEO4J_URI")
         self.auth = (os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
-
+        self.database = "neo4j"
 
     def check_connectivity(self) -> None:
         with GraphDatabase.driver(self.uri, auth=self.auth) as driver:
             driver.verify_connectivity()
 
+    def execute_write(self, cypher):
+        with GraphDatabase.driver(self.uri, auth=self.auth) as driver:
+            with driver.session(database=self.database) as session:
+                session.execute_write(_execute_write_tx, cypher)
+                print("successful")
+
+    def execute_query(self, query: str, **kwargs: any):
+        with GraphDatabase.driver(self.uri, auth=self.auth) as driver:
+            records, summary, keys = driver.execute_query(
+            query,
+            **kwargs,
+            database_=self.database,
+            )
+        return records, summary, keys
+
+    def clear_db(self):
+        with GraphDatabase.driver(self.uri, auth=self.auth) as driver:
+            with driver.session(database=self.database) as session:
+                session.execute_write(_clear_db_tx)
+                print("cleared all nodes in " + self.database)
+
+def _execute_write_tx(tx, cypher):
+    tx.run(cypher) 
+    
+def _clear_db_tx(tx):
+    # delete all nodes with relationships
+    tx.run("""
+        MATCH (a) -[r] -> () DELETE a, r
+    """) 
+    # delete nodes that have no relationships
+    tx.run("""
+        MATCH (a) DELETE a
+    """)
--- 00_src/tests/knowlegde_graph/test_knowledge_graph_neo4j.py
+++ 00_src/tests/knowlegde_graph/test_knowledge_graph_neo4j.py
@@ -1,7 +1,44 @@
+import pytest
 from knowledge_graph.knowledge_graph_neo4j import KnowledgeGraphNeo4J
 
 # set up
 testee = KnowledgeGraphNeo4J()
 
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    # Before each
+    # Do nothing
+    yield
+    # After each
+    testee.clear_db()
+
 def test_check_connectivity():
     testee.check_connectivity()
+
+def test_execute_write():
+    testee.execute_write("""
+    CREATE (a:TestNode { Tag : "This is a test" })
+    """)
+
+def test_execute_query():
+    testee.execute_write("""
+    CREATE (a:TestNode { Tag : "This is a test" })
+    """)
+    records, summary, keys = testee.execute_query("""
+                                MATCH (n:TestNode) RETURN n LIMIT $limit
+                                """,
+                                limit = 25)
+    assert len(records) == 1
+
+def test_clear_db():
+    testee.execute_write("""
+    CREATE (a:TestNode { Tag : "This is a test" })
+    """)
+
+    testee.clear_db()
+
+    records, summary, keys = testee.execute_query("""
+                                MATCH (n:TestNode) RETURN n LIMIT $limit
+                                """,
+                                limit = 25)
+    assert len(records) == 0
