f34abfebaafc19bebc8ce31bc51968b99dba9135
RogNaef <rognaef@gmail.com>
2025-06-19 16:07:52 +0200

    Added decay to update

--- 00_src/knowledge_graph/path_graph.py
+++ 00_src/knowledge_graph/path_graph.py
@@ -9,21 +9,24 @@ class PathGraph(GraphInterface):
     trajectory : list[int]
     max_id: int
     node_id : int
+    parent_id : int
     reward : float
 
     root_parent_id = 0
     learning_rate = 0.1
+    decay = 0.9
 
     def __init__(self, client: Neo4jClient) -> None:
         self.client = client
         self.trajectory = []
         self.max_id = 1
         self.node_id = 1
+        self.parent_id = self.root_parent_id
         self.reward = 0
         self._init_path()
 
     def _init_path(self):
-        cypher = "CREATE (:Path {{id: {id}, parent_id:{parent_id}, possible_actions:{possible_actions}, trajectory:{trajectory}, reward:{reward}, value:{value}, done:{done}, caption:\"{caption}\"}});".format(id=self.node_id, parent_id=self.root_parent_id, possible_actions=self._get_possible_actions(), trajectory=self.trajectory, reward=0, value=0, done=False, caption="Root")
+        cypher = "CREATE (:Path {{id: {id}, parent_id:{parent_id}, possible_actions:{possible_actions}, trajectory:{trajectory}, reward:{reward}, value:{value}, done:{done}, caption:\"{caption}\"}});".format(id=self.node_id, parent_id=self.parent_id, possible_actions=self._get_possible_actions(), trajectory=self.trajectory, reward=0, value=0, done=False, caption="Root")
         self.client.write(cypher)
 
     def _get_possible_actions(self) -> list[int]:
@@ -33,13 +36,13 @@ class PathGraph(GraphInterface):
     def step(self, action:int, reward:float, done:bool) -> None:
         # update values
         self.trajectory.append(action)
-        parent_id = self.node_id
+        self.parent_id = self.node_id
         self.node_id = self._create_next_id()
         self.reward+=reward
 
         # create new node
         caption = "Solution" if done else "Path"
-        cypher = "CREATE (:Path {{id: {id}, parent_id:{parent_id}, possible_actions:{possible_actions}, trajectory:{trajectory}, reward:{reward}, value:{value}, done:{done}, caption:\"{caption}\"}});".format(id=self.node_id, parent_id=parent_id, possible_actions=self._get_possible_actions(), trajectory=self.trajectory, reward=self.reward, value=self.reward, done=done, caption=caption)
+        cypher = "CREATE (:Path {{id: {id}, parent_id:{parent_id}, possible_actions:{possible_actions}, trajectory:{trajectory}, reward:{reward}, value:{value}, done:{done}, caption:\"{caption}\"}});".format(id=self.node_id, parent_id=self.parent_id, possible_actions=self._get_possible_actions(), trajectory=self.trajectory, reward=self.reward, value=self.reward, done=done, caption=caption)
         self.client.write(cypher)
 
         # create relationship
@@ -47,7 +50,7 @@ class PathGraph(GraphInterface):
         self.client.write("""   MATCH (p1:Path),(p2:Path)
                                 WHERE p1.id = {parent_id} AND p2.id = {id}
                                 CREATE (p1)-[:MOVE {{id:{action}, caption:\"{caption}\"}}]->(p2);
-                        """.format(parent_id=parent_id, id=self.node_id, action=action, caption=caption))
+                        """.format(parent_id=self.parent_id, id=self.node_id, action=action, caption=caption))
         
     def _create_next_id(self) -> int:
         self.max_id+=1
@@ -63,12 +66,12 @@ class PathGraph(GraphInterface):
         self.reward = path_node.get("reward")
 
     def backprop(self, sim_value:float):
-        current_id = copy.deepcopy(self.node_id)
+        current_id = copy.deepcopy(self.parent_id)
         value = copy.deepcopy(sim_value)
 
         while current_id is not self.root_parent_id:
-            value = value * self.learning_rate
-            records, summary, keys = self.client.read("MATCH (p:Path) WHERE p.id = {id} SET p += {{value: p.value + {value}}} return p".format(id=current_id, value=value))
+            value = value * self.decay
+            records, summary, keys = self.client.read("MATCH (p:Path) WHERE p.id = {id} SET p += {{value: p.value + ({learning_rate} * {value})}} return p".format(id=current_id, learning_rate=self.learning_rate, value=value))
             if len(records) != 1:
                 raise Exception("Path node with id {id} not found".format(id=current_id))
             current_id = records[0]["p"]["parent_id"]
