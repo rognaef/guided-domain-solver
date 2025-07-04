0ca691bc3716c2badf0dcd035eb904e01e76bd8d
RogNaef <rognaef@gmail.com>
2025-07-04 20:27:42 +0200

    Added reproducibility test for random sampling

--- 00_src/tests/mcts/test_mcts.py
+++ 00_src/tests/mcts/test_mcts.py
@@ -5,6 +5,7 @@ from mcts.simulation import simulation
 from mcts.backprop import backprop
 from environment.environment import SokobanEnvImpl
 import numpy as np
+import random
 
 def test_solve():
     testee = Builder().build()
@@ -22,6 +23,27 @@ def test_solve():
 
 def test_solve_random_sampling():
     testee = Builder().setSelection(selection).setExpansion(expansion_random_sampling).setSimulation(simulation).setBackprop(backprop).build()
-    env = SokobanEnvImpl(use_default_env=True)
-    trajectory, num_explored_nodes, needed_time = testee.solve(env)
-    assert len(trajectory) == 30
+    env = SokobanEnvImpl(fixated_env=(
+        np.array([[0, 0, 0, 0, 0, 0],
+                  [0, 1, 0, 0, 0, 0],
+                  [0, 1, 1, 1, 2, 0],
+                  [0, 2, 1, 1, 2, 0],
+                  [0, 1, 1, 1, 2, 0],
+                  [0, 0, 0, 0, 0, 0]]),
+        np.array([[0, 0, 0, 0, 0, 0],
+                  [0, 5, 0, 0, 0, 0],
+                  [0, 4, 4, 1, 2, 0],
+                  [0, 2, 1, 4, 2, 0],
+                  [0, 1, 1, 4, 2, 0],
+                  [0, 0, 0, 0, 0, 0]]),
+        {(2,4):(2,2), (3,1):(2,1), (3,4):(3,3), (4,4):(4,3)}
+    ))
+    random.seed(42)
+    trajectory_1, num_explored_nodes_1, needed_time_1 = testee.solve(env)
+    assert len(trajectory_1) == 9
+
+    env.reset()
+    random.seed(42)
+    trajectory_2, num_explored_nodes_2, needed_time_2 = testee.solve(env)
+    assert len(trajectory_2) == 9
+    assert num_explored_nodes_1 == num_explored_nodes_2
